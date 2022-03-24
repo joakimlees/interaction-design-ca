5c43a4d34a9d77d1d72d2cb3f9729993873507cc
joakimlees <91544170+joakimlees@users.noreply.github.com>
2022-03-24 22:40:19 +0100

    added local storage

--- js/details.js
+++ js/details.js
@@ -4,25 +4,92 @@ const detailsContainer = document.querySelector(".main-golden-age");
 
 const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
-const characterId = params.get("id");
+const jacketId = params.get("id");
 
 const findId = products.filter((product) => {
-  return product.id === characterId;
+  return product.id === jacketId;
 });
+
+const jacket = findId[0];
 
 detailsContainer.innerHTML = `
 
 <div>
 <div class="product-wrapper">
   <div class="image-container">
-    <img src="${findId[0].image}" />
+    <img src="${jacket.image}" />
   </div>
   <div class="product-text">
-    <h2>${findId[0].brand}</h2>
-    <h3>${findId[0].name}</h3>
-    <p>${findId[0].model}</p>
-    <p class="price-tag">$${findId[0].price}</p>
+    <h2>${jacket.brand}</h2>
+    <h3>${jacket.name}</h3>
+    <p>${jacket.type}<p>
+    <p>${jacket.model}</p>
+    <p class="price-tag">$${jacket.price}</p>
+    <form>
+    <label for=${jacket.color[0]}>${jacket.color[0]}<label>
+    <input type="radio" id="${jacket.color[0]}" value="${jacket.color[0]}" name="color" checked="checked">
+    <label for=${jacket.color[1]}>${jacket.color[1]}<label>
+    <input type="radio" id="${jacket.color[1]}" value="${jacket.color[1]}" name="color">
+    <label for=${jacket.color[2]}>${jacket.color[2]}<label>
+    <input type="radio" id="${jacket.color[2]}" value="${jacket.color[2]}" name="color">
+    <label for="size-select">Select size:</label>
+    <select name="size-select" id="size-select">
+      <option value="${jacket.size[0]}" selected="selected">${jacket.size[0]}</option>
+      <option value="${jacket.size[1]}">${jacket.size[1]}</option>
+      <option value="${jacket.size[2]}">${jacket.size[2]}</option>
+    </select>
+    <button class="add-cart-btn" data-color="${jacket.color[0]}" data-size="${jacket.size[0]}">add to local storage</button>
+    </form>
   </div>
 </div>
 </a>
 </div>`;
+
+const addCartButton = document.querySelector(".add-cart-btn");
+
+const colorSelects = document.querySelectorAll("input[name='color']");
+for (let i = 0; i < colorSelects.length; i++) {
+  colorSelects[i].addEventListener("click", onColorSelect);
+}
+
+function onColorSelect(event) {
+  addCartButton.dataset.color = event.target.value;
+}
+
+const sizeSelects = document.querySelectorAll("select[name='size-select'] option");
+for (let i = 0; i < sizeSelects.length; i++) {
+  sizeSelects[i].addEventListener("click", onSizeSelect);
+}
+
+function onSizeSelect(event) {
+  addCartButton.dataset.size = event.target.value;
+}
+
+addCartButton.addEventListener("click", orderDetails);
+
+function orderDetails(event) {
+  event.preventDefault();
+
+  const color = this.dataset.color;
+  const size = this.dataset.size;
+
+  const currentProd = getProduct();
+  const storedProdDetails = { size: size, color: color };
+
+  currentProd.push(storedProdDetails);
+
+  saveProducts(currentProd);
+}
+
+function getProduct() {
+  const storedProducts = localStorage.getItem("products");
+
+  if (!storedProducts) {
+    return [];
+  }
+  return JSON.parse(storedProducts);
+}
+
+function saveProducts(storedProducts) {
+  localStorage.setItem("products", JSON.stringify(storedProducts));
+}
